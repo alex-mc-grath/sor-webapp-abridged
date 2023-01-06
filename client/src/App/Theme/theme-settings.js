b69207b0ea41c8718ea5aa0b63dcbfef9016b574
alex-mc-grath <a.m.courchesne1@gmail.com>
2023-01-06 16:52:24 -0500

    test

--- client/src/App/Theme/theme-settings.js
+++ client/src/App/Theme/theme-settings.js
@@ -1,29 +1,27 @@
-// https://color-hex.org/color/08a2e5
 export const themeSettings = () => ({
-    colors: {
-  
-      primary: '#418EEB',
-      secondary: '#08a2e5',
-      tertiary: '#2c2c2d',
+  colors: {
 
-      black: '#28282B',
-      secondaryShade1:'#01202D',
-      secondaryShade2:'#001016',
-  
-      body: '#1C1C1E',
-      light: '#E5E5E5',
+    primary: '#418EEB',
+    secondary: '#08a2e5',
+    tertiary: '#2c2c2d',
 
-      text: '#F78154',
+    black: '#28282B',
+    secondaryShade1: '#01202D',
+    secondaryShade2: '#001016',
 
-      fadedText: '#67666a',
-      highlight: '#FFE07D',
-      bright: '#F8C7CC',
-      underline: '#247BA0',
-      
-      danger: '#DA2C38',
-      warning: 'yellow',
-      success: '#B3E59F',
-    },
-    transitionSpeed: '400ms',
-  });
-  
+    body: '#1C1C1E',
+    light: '#E5E5E5',
+
+    text: '#F78154',
+
+    fadedText: '#67666a',
+    highlight: '#FFE07D',
+    bright: '#F8C7CC',
+    underline: '#247BA0',
+
+    danger: '#DA2C38',
+    warning: 'yellow',
+    success: '#B3E59F',
+  },
+  transitionSpeed: '400ms',
+});
